2741fc28988e26663b3df52b37aa44c23be130d3
Kakoha <dhiamok1@gmail.com>
2024-04-29 11:29:53 +0100

    added image viewer

--- src/main/java/com/example/imgur/HelloController.java
+++ src/main/java/com/example/imgur/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.imgur;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private ImageView imageView;
+
+    @FXML
+    private TextField tfUrl;
+
+    private String imageurl;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        imageurl = tfUrl.getText();
+        Image image = new Image(imageurl,false);
+        imageView.setImage(image);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        imageurl = tfUrl.getText();
+        Image image = new Image(imageurl,false);
+        imageView.setImage(image);
     }
 }
